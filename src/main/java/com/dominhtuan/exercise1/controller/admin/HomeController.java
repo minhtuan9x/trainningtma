f01fc9e17deb3482d5e538330a0d1bf3888a8916
dmtuan2 <0mtmtmt@gmail.com>
2022-03-23 11:44:56 +0700

    stage 1

---
+++ src/main/java/com/dominhtuan/exercise1/controller/admin/HomeController.java
@@ -0,0 +1,15 @@
+package com.dominhtuan.exercise1.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public ModelAndView home(){
+        return new ModelAndView("redirect:/admin/book/list");
+    }
+}
